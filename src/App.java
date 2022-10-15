375eb0762b8351a9ec45000ff6b12a0ba34dde73
NicoAL95 <nicoabellaia@gmail.com>
2022-10-15 22:08:21 +0700

    Recreate Base System

--- src/App.java
+++ src/App.java
@@ -1,15 +1,123 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-        Sport sp = new Sport();
 
-        System.out.println("Aplikasi Penjual Motor Gede | Nico Abel Laia - TI Matana 2021");
-        System.out.println("Brand   : " + sp.bikeBrand);
-        System.out.println("Type    : " + sp.getBikeType());
-        System.out.println("Name    : " + sp.getBikeName());
-        System.out.println("Series  : " + sp.getBikeSeries());
-        System.out.println("CC      : " + sp.getBikeCc());
-        System.out.println("Power   : " + sp.getBikePower());
-        System.out.println("Torque  : " + sp.getBikeTorque());
+        // Escaper
+        int esc = 0;
+        int acc = 0;
+        int back = 1;
 
+        // Arrays
+        Dealer [] main = new Dealer[10];
+
+        // Fill Array Main Harga to 0
+        for (int i = 0; i < main.length; i++) {
+            Dealer temp = new Dealer();
+            temp.setFirstStart();
+            main[i] = temp;
+        }
+    
+        Scanner input = new Scanner(System.in);
+
+        
+
+        do {
+
+            System.out.println("Program Input Data Dealer Motor: ");
+            System.out.println("1.Masukkan Data\n2. Cek Daftar\n3. Komparasi Kendaraan\n4. Hapus Data Kendaraan");
+
+            System.out.print("Pilihan Anda: ");
+            int inputUser = input.nextInt();
+            System.out.println("Input User:  " + inputUser);
+            switch (inputUser) {
+                case 1:
+
+                // Clear terminal
+                System.out.print("\033\143");
+
+                // Restart Token
+                esc = 0;
+                acc = 0;
+
+                    // Token Checker
+                    for (int i = 0; i < main.length; i++) {
+                        if (main[i].getHarga() == 0) {
+                            esc++;
+                        } else {
+                            acc++;
+                        }
+                    }
+
+                    if (esc < 11) {
+
+                        System.out.println("Jumlah Input Maksimal: " + esc);
+
+                        System.out.print("Masukkan Jumlah Kendaraan: ");
+
+                        // Confirm Input Access
+                        int inputNew = 0;
+                        do {
+                            int tryIn = input.nextInt();
+                            inputNew = tryIn;
+                        } while (inputNew > esc);
+                        
+                        if (acc == 0) {
+                            for (int i = 0; i < inputNew; i++) {
+                                Dealer di = new Dealer();
+                                di.setNamaMotor();
+                                di.setHargaMotor();
+                                // Reintialize Arrays
+                                main[i] = di;
+                            }
+                        } else {
+                            for (int i = acc; i < inputNew; i++) {
+                                Dealer di = new Dealer();
+                                di.setNamaMotor();
+                                di.setHargaMotor();
+                                // Reintialize Arrays
+                                main[i] = di;
+                            }
+                        }
+
+                    } else {
+                        System.out.print("Maaf data sudah penuh!");
+                    }
+
+                    break;
+
+                case 2:
+                // Clear terminal
+                System.out.print("\033\143");
+
+                    // Restart Token
+                    esc = 0;
+                    acc = 0;
+
+                        // Token Checker
+                        for (int i = 0; i < main.length; i++) {
+                            if (main[i].getHarga() == 0) {
+                                esc++;
+                            } else {
+                                acc++;
+                            }
+                        }
+
+                    for (int i = 0; i < acc; i++) {
+                        System.out.println("Nama motor : " + main[i].getNama() + " Harga motor : " + main[i].getHarga());
+                    }   
+                    
+                    break;
+            
+                default:
+                // Clear terminal
+                System.out.print("\033\143");
+                    break;
+            }     
+
+        } while (back == 1);
+
+        input.close();
+        
     }
 }
--- src/Dealer.java
+++ src/Dealer.java
@@ -1,9 +1,37 @@
-public class Dealer {
-    
-    // Jenis Motor
-    protected String bikeBrand = "Ducati";
-    protected String bikeType = "Big Bike"; 
-    protected String bikeCountry = "Italia";
+import java.util.Scanner;
 
+public class Dealer {
+
+    private Scanner inputDataDealer;
+
+    private String nama = "";
+    private int harga = 0;
+
+    public Dealer() {
+        inputDataDealer = new Scanner(System.in);
+    }
+
+    public int setFirstStart(){
+        this.harga = 0; 
+        return this.harga;
+    }
+
+    public void setNamaMotor(){
+        System.out.print("Nama Motor: ");
+        this.nama = inputDataDealer.nextLine();
+    }
+
+    public void setHargaMotor(){
+        System.out.print("Harga Motor: ");
+        this.harga = inputDataDealer.nextInt();
+    }
+
+    public String getNama(){
+        return this.nama;
+    }
+
+    public int getHarga(){
+        return this.harga;
+    }
 
 }
